660c48e69ee825c01ad3239a5fe97b1e041de355
ganesh <ganeshturate1.com>
2025-03-13 14:33:03 +0530

    updated interview component

--- CodeChallenge/src/app/pages/live-challenge/live-challenge.component.ts
+++ CodeChallenge/src/app/pages/live-challenge/live-challenge.component.ts
@@ -129,7 +129,7 @@ export class LiveChallengeComponent
         this.socketService.sendData("stream", frameData);
 
         // Repeat every 100ms
-        setTimeout(sendFrame, 100);
+        setTimeout(sendFrame, 20);
       };
 
       sendFrame()
--- CodeChallenge/src/app/pages/live-interview/live-interview.component.ts
+++ CodeChallenge/src/app/pages/live-interview/live-interview.component.ts
@@ -77,7 +77,7 @@ document.addEventListener('paste', (event) => event.preventDefault());
       }
     }
 
-    setTimeout(() => this.displayRemoteVideo(), 100); // Refresh every 100ms
+    setTimeout(() => this.displayRemoteVideo(), 20); // Refresh every 100ms
   }
 
   toggleModal(){
